ffcddf3e8e9ae8dd03a42f1317692c2f5005db1a
My Dear Sir <saints.fanatic@yahoo.com>
2017-11-24 17:26:53 +0000

    Comment between checker function

--- between.s
+++ between.s
@@ -8,18 +8,18 @@ LFB0:
 	pushl	%ebp
 	.cfi_def_cfa_offset 8
 	.cfi_offset 5, -8
-	movl	%esp, %ebp
+	movl	%esp, %ebp		# set base pointer
 	.cfi_def_cfa_register 5
-	movl	8(%ebp), %eax
-	cmpl	12(%ebp), %eax
-	jle	L2
-	movl	8(%ebp), %eax
-	cmpl	16(%ebp), %eax
-	jge	L2
-	movl	$1, %eax
-	jmp	L3
+	movl	8(%ebp), %eax	# put first argument in eax
+	cmpl	12(%ebp), %eax	# check second argument less than first
+	jle	L2					# if last line set 'less than' flag, set return value false
+	# first argument is already in eax
+	cmpl	16(%ebp), %eax	# check if third argument is less than first
+	jge	L2					# if last line set 'greater than' or 'equal', set return false
+	movl	$1, %eax		# when code gets this far, return true
+	jmp	L3					# go to return
 L2:
-	movl	$0, %eax
+	movl	$0, %eax		# set return value to false
 L3:
 	popl	%ebp
 	.cfi_restore 5
